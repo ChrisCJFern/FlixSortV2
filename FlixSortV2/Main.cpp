0f71de8712d774c40036160460989326d0db2b6c
sgilman00 <s.gilman@ufl.edu>
2020-11-12 00:26:22 -0500

    menu

--- FlixSortV2/Main.cpp
+++ FlixSortV2/Main.cpp
@@ -31,7 +31,7 @@ vector<int> menuPrint() {
 	cout << "|     How would you like to base your search?    |" << endl;
 	cout << "|Enter the numbers of the choices you would like |" << endl;
 	cout << "|to search by in the following format: # # # ..  |" << endl;
-	cout << "|1. Time (in minutes)                            |" << endl;
+	cout << "|1. Length of time                               |" << endl;
 	cout << "|2. Company                                      |" << endl;
 	cout << "|3. Genre                                        |" << endl;
 	cout << "|4. Rating                                       |" << endl;
@@ -66,6 +66,58 @@ vector<int> menuPrint() {
 	return choices;
 }
 
+void runChoices(vector<int> choices) {
+	for (int i = 0; i < choices.size(); i++) {
+		cout << choices[i] << ". ";
+		switch (choices[i]) {
+		case 1:
+			cout << "How much time do you have? (in minutes)" << endl;
+			break;
+		case 2:
+			cout << "Production Company" << endl;
+			//list of options from a map or set?									//do methods for each here?
+			break;
+		case 3:
+			cout << "Genre: " << endl;
+			//menu list of options from a set ?
+			break;
+		case 4:
+			cout << "Rating" << endl;
+			break;
+		case 5:
+			cout << "Release Year" << endl;
+			break;
+		case 6:
+			cout << "Director" << endl;
+			break;
+		case 7:
+			cout << "Starring actor" << endl;
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+void searchAgain() {
+	string input;
+	vector<int> choices;
+	do {
+		cout << setfill('=') << setw(51);																		//not sure if this should be a function to be called...
+		cout << "\n";
+		cout << "|        Would you like to search again?         |" << endl;
+		cout << "|                 Enter: Y or N                  |" << endl;
+		cout << setfill('=') << setw(51);
+		cout << "\n";
+		cin >> input;
+		if (input == "Y" || input == "y") {
+			choices = menuPrint();
+			runChoices(choices);
+		}
+	}
+	while (input == "Y" || input == "y");
+	cout << "Enjoy the movie!" << endl;
+}
 
 int main() {
 	//---------------------PRINTS INITIAL MENU---------------------------------------
@@ -77,12 +129,66 @@ int main() {
 	cout << "|1. Want to watch a movie marathon?              |" << endl;
 	cout << "|2. Can't decide what movie to watch?            |" << endl;
 	cout << "|             We can choose for you!             |" << endl;
+
 	vector<int> choices;
 	choices = menuPrint();
-	for (int i = 0; i < choices.size(); i++) {
-		cout << choices[i] << " ";
+	runChoices(choices);
+	searchAgain();
+	
+	string line;
+	ifstream movieFile;
+	movieFile.open("movies.csv", ios::in);
+	if (movieFile.is_open()) {
+		getline(movieFile, line);
+		cout << line << endl;
 	}
-	//------------------------SEARCH AGAIN---------------------------------------------
+	movieFile.close();
+
+	return 0;
+}
+
+
+/*for (int i = 0; i < choices.size(); i++) {
+	cout << choices[i] << ". ";
+	switch (choices[i]) {
+	case 1:
+		cout << "How much time do you have? (in minutes)" << endl;
+		break;
+	case 2:
+		cout << "Production Company" << endl;
+					//list of options from a map or set?									//do methods for each here?
+		break;
+	case 3:
+		cout << "Genre: " << endl;
+					//menu list of options from a set ?
+		break;
+	case 4:
+		cout << "Rating" << endl;
+		break;
+	case 5:
+		cout << "Release Year" << endl;
+		break;
+	case 6:
+		cout << "Director" << endl;
+		break;
+	case 7:
+		cout << "Starring actor" << endl;
+		break;
+	}
+}*/
+/*------------------------SEARCH AGAIN---------------------------------------------
+//cout << endl;
+cout << setfill('=') << setw(51);																		//not sure if this should be a function to be called...
+cout << "\n";
+cout << "|        Would you like to search again?         |" << endl;
+cout << "|                 Enter: Y or N                  |" << endl;
+cout << setfill('=') << setw(51);
+cout << "\n";
+string input;
+cin >> input;
+while (input == "Y" || input == "y") {
+	choices = menuPrint();
+	runChoices(choices);
 	cout << endl;
 	cout << setfill('=') << setw(51);
 	cout << "\n";
@@ -90,37 +196,7 @@ int main() {
 	cout << "|                 Enter: Y or N                  |" << endl;
 	cout << setfill('=') << setw(51);
 	cout << "\n";
-	string input;
 	cin >> input;
-	while (input == "Y" || input == "y") {
-		choices = menuPrint();
-		for (int i = 0; i < choices.size(); i++) {
-			cout << choices[i] << " ";
-		}
-		cout << endl;
-		cout << setfill('=') << setw(51);
-		cout << "\n";
-		cout << "|        Would you like to search again?         |" << endl;
-		cout << "|                 Enter: Y or N                  |" << endl;
-		cout << setfill('=') << setw(51);
-		cout << "\n";
-		cin >> input;
-	}
-	cout << "Enjoy the movie!" << endl;
-	////////////////////////////////////////////////////////////////////////////////////
-
-	string line;
-	ifstream movieFile;
-	movieFile.open("movies.csv", ios::in);
-	while (movieFile.is_open()) {
-		getline(movieFile, line);
-		cout << line << endl;
-	
-	}
-	movieFile.close();
-	
-
-	return 0;
 }
-
-
+cout << "Enjoy the movie!" << endl;*/
+////////////////////////////////////////////////////////////////////////////////////
