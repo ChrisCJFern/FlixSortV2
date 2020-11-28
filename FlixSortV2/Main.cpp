2e08a12ea0060e9b56adb5286c584c8bb19c883c
sgilman00 <s.gilman@ufl.edu>
2020-11-28 14:45:46 -0500

    insert

--- FlixSortV2/Main.cpp
+++ FlixSortV2/Main.cpp
@@ -465,7 +465,6 @@ Node* balance(Node* node) {
 	return node;
 }
 
-/*
 Node* insertNameId(Node* node, movie name, double id) {                           //insert a node, by taking in a root node, a string for the name and an int for the id
 	if (node == nullptr) {														//if node is nullptr, make a new node, else go to the left or right depending on how id compares to node value
 		Node* temp = new Node(id, name);										//then set node's left or right equal to the result of calling insert on the node->left or node->right
@@ -480,7 +479,7 @@ Node* insertNameId(Node* node, movie name, double id) {                         
 		node->right = insertNameId(node->right, name, id);
 	}
 	return balance(node);                                   //balance node and return resulting root
-}*/
+}
 
 
 void recalcBalanceFactor(Node* node) {
@@ -495,110 +494,6 @@ void recalcBalanceFactor(Node* node) {
 //if node is nullptr, make a new node, else go to the left or right depending on how id compares to node value
 //then set node's left or right equal to the result of calling insert on the node->left or node->right
 // inserts if year and genre match, if they do, add in rating/score value and movie name
-Node* insertNameId(Node* node, int year1, int year2, string genre) {  
-	//Node* root = new Node();
-	// takes in movies.csv file
-	string line;
-	ifstream movieFile;
-	movieFile.open("movies.csv", ios::in);
-	int numLine = 0;
-	if (movieFile.is_open()) {
-		getline(movieFile, line);
-		while (getline(movieFile, line)) {
-			numLine++;
-		}
-		movieFile.close();
-	}
-	string _budget, _company, _country, _director, _genre, _gross, _name, _rating;
-	string _released, _runtime, _score, _star, _votes, _writer, _year;
-	movieFile.open("movies.csv", ios::in);
-	getline(movieFile, line);
-	for (int i = 0; i < numLine; i++) {
-		// first take in the budget
-		getline(movieFile, _budget, ',');
-		// then the company name
-		char d;
-		d = movieFile.get();
-		if (char(d) == '"') {
-			getline(movieFile, _company, '"');
-			_company = _company.substr(0, _company.length() - 1);
-			getline(movieFile, line, ',');
-		}
-		else {
-			movieFile.putback(d);
-			getline(movieFile, _company, ',');
-		}
-		// then the country, director, genre, and gross
-		getline(movieFile, _country, ',');
-		getline(movieFile, _director, ',');
-		getline(movieFile, _genre, ',');
-		getline(movieFile, _gross, ',');
-		// then the name
-		d = movieFile.get();
-		if (char(d) == '"') {
-			getline(movieFile, _name, '"');
-			_name = _name.substr(0, _name.length() - 2);
-			getline(movieFile, line, ',');
-		}
-		else {
-			movieFile.putback(d);
-			getline(movieFile, _name, ',');
-		}
-		// then the rating, released date, runtime, score, star, votes, writer, year
-		getline(movieFile, _rating, ',');
-		getline(movieFile, _released, ',');
-		getline(movieFile, _runtime, ',');
-		getline(movieFile, _score, ',');
-		getline(movieFile, _star, ',');
-		getline(movieFile, _votes, ',');
-		getline(movieFile, _writer, ',');
-		getline(movieFile, _year);
-		// adds values into movie structure
-		movie temp(stoi(_budget), _company, _country, _director, _genre, stoi(_gross), _name, _rating, _released, stoi(_runtime),
-			stod(_score), _star, stoi(_votes), _writer, stoi(_year));
-
-		// begins inserting nodes into AVL Tree
-
-		// only inserts movies based on the preferred genre and year range
-		// if there is no node, make new node
-		if (_genre == genre && (stoi(_year) >= year1) && (stoi(_year) <= year2)) {
-			if (node == nullptr) {
-				// adds rating and movie name into node
-				Node* tempNode = new Node(stod(_score), temp);
-				node = tempNode;
-				return node;
-				delete tempNode;
-			}
-			// if score is less than node's val, traverse through the tree on the left
-			if (stod(_score) <= node->val) {
-				node->left = insertNameId(node->left, year1, year2, genre);
-			}
-			// if score is greater than node's val, traverse through the tree on the right
-			else if (stod(_score) > node->val) {
-				node->right = insertNameId(node->right, year1, year2, genre);
-			}
-		}
-		return balance(node);
-	}
-	movieFile.close();
-	
-	//balance node and return resulting root
-	return node;                             // idk if i need to include this in the for loop      
-}
-
-// prints the AVL Tree in inorder order
-void printInorder(Node* node) {						//place the order of the nodes using In order traversal in a string
-	if (node == nullptr) {												//Right
-		return;														     //Node
-	}																	//Left
-	else {
-		printInorder(node->right);
-		cout << node->name.name << " | " << node->name.director << " | " << node->name.runtime << " minutes" << endl;
-		printInorder(node->left);
-	}
-}
-
-/*
 Node* createTree(Node* tree, string genre, int year1, int year2, vector<string> ratings) {
 	Node* root = new Node;
 	string line;
@@ -683,7 +578,7 @@ void printInorder(Node* node) {
 		printInorder(node->left);
 	}
 }
-*/
+
 
 int main() {
 	unordered_map<int, string> genre = { {97, "Action"}, {98, "Adventure"}, {99, "Animation"}, {100, "Biography"}, {101, "Comedy"}, {102, "Crime"}, {103, "Drama"}, {104, "Family"}, {105, "Fantasy"}, {106, "Horror"}, {107, "Musical"}, {108, "Mystery"}, {109, "Romance"}, {110, "Sci-Fi"}, {111, "Thriller"}, {112, "War"}, {113, "Western"} };
@@ -719,8 +614,8 @@ int main() {
 					cout << "Here is a list of " << genre[choice] << " movies from the year " << year[choice2].first << " to " << year[choice2].second << "." << endl;
 					m1 = createMap(genre[choice], year[choice2].first, year[choice2].second, rating[choice3]);
 					printMap(m1);
-					//tree = createTree(tree, genre[choice], year[choice2].first, year[choice2].second, rating[choice3]);
-					//printInorder(tree);
+					tree = createTree(tree, genre[choice], year[choice2].first, year[choice2].second, rating[choice3]);
+					printInorder(tree);
 					/*cout << "How much time do you have? Enter in minutes: ";     //testing how to marathon?
 					cin >> mins;
 					vector<movie> movies;
