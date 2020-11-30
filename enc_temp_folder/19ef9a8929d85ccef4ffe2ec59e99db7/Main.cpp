3f4988fab68f044817e3c094ed3202dfd084176f
sgilman00 <s.gilman@ufl.edu>
2020-11-30 17:23:15 -0500

    small output changes

--- enc_temp_folder/19ef9a8929d85ccef4ffe2ec59e99db7/Main.cpp
+++ enc_temp_folder/19ef9a8929d85ccef4ffe2ec59e99db7/Main.cpp
@@ -694,14 +694,14 @@ void printMarathon(unordered_multimap<string, movie> m1) {
 		try {
 			if (stod(hour)) {
 				int mins = 60 * stod(hour);
-				vector<movie*> movies;
+				vector<movie> movies;
 				movies = marathon(mins, m1);
 				int time = 0;
 				int counter = 1;
 				for (int i = 0; i < movies.size(); i++) {
-					cout << counter << ". " << movies[i]->name << endl;
+					cout << counter << ". " << movies[i].name << endl;
 					counter++;
-					time += movies[i]->runtime;
+					time += movies[i].runtime;
 				}
 				cout << "The total time it will take you to watch these movies is: ";
 				minutesToHours(time);
@@ -713,6 +713,7 @@ void printMarathon(unordered_multimap<string, movie> m1) {
 			cout << "Invalid input. Please enter Y or N: ";
 		}
 	}
+	
 }
 
 int main() {
